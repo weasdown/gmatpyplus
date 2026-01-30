ba067b4342a4c9a128bbaac74d398729b8d34302
weasdown <34220924+weasdown@users.noreply.github.com>
2026-01-30 16:33:04 +0000

    test: use TestTank._test_tank() in test_chemical_tank()

--- tests/test_tanks.py
+++ tests/test_tanks.py
@@ -30,28 +30,8 @@ class TestTanks(unittest.TestCase):
                                                                    f'({tank.direction}).')
 
     def test_chemical_tank(self):
-        fuel_com: np.ndarray = np.array([0, 0.1, 0.2])
-        fuel_moi: np.ndarray = np.array([0.2, 0.1, 0])
-        direction = np.array([1, 0, 0])
-        second_direction = np.array([0, 1, 0])
-
-        # ct1: gp.ChemicalTank = gp.ChemicalTank('CT1')
         ct1: gp.ChemicalTank = gp.ChemicalTank('CT1', 500, True, 1000, 10, 10, 0.5, 1000, gp.PressureModel.BlowDown,
-                                               fuel_com, fuel_moi,
-                                               direction,
-                                               second_direction)
+                                               self.fuel_com, self.fuel_moi, self.direction, self.second_direction,
+                                               self.hw_origin_in_bcs)
 
-        ct1.Help()
-
-        # Test the gp.ChemicalTank's direction matches the direction argument.
-        self.assertTrue((direction == ct1.direction).all(), f'gp.ChemicalTank direction attribute ({ct1.direction})'
-                                                            f' is not equal to direction argument ({direction}).')
-
-        # Test the gp.ChemicalTank's direction matches GMAT's internal direction values.
-        direction_x_field = float(ct1.GetField('DirectionX'))
-        direction_y_field = float(ct1.GetField('DirectionY'))
-        direction_z_field = float(ct1.GetField('DirectionZ'))
-        direction_field = np.array([direction_x_field, direction_y_field, direction_z_field])
-        self.assertTrue((direction_field == ct1.direction).all(), f'direction_field from GMAT object '
-                                                                  f'({direction_field}) is not equal to direction argument '
-                                                                  f'({ct1.direction}).')
+        self._test_tank(ct1)
