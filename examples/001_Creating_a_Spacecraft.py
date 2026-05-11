148482e62bdcb83e6a25473fbe9a8dc08f2d4b91
weasdown <34220924+weasdown@users.noreply.github.com>
2026-05-11 10:26:41 +0100

    test: add multiple tanks to sat_params for testing

--- examples/001_Creating_a_Spacecraft.py
+++ examples/001_Creating_a_Spacecraft.py
@@ -1,5 +1,22 @@
 import gmatpyplus as gp
 
+# sat_params = {
+#     'Name': 'DefaultSC',
+#     'Orbit': {
+#         'CoordSys': 'EarthMJ2000Eq',
+#         'Epoch': '21545',
+#         'DisplayStateType': 'Keplerian',
+#         'SMA': 7200
+#     },
+#     'DryMass': 100,  # kg
+#     'Hardware': {'ChemicalTanks': [{'Name': 'ChemicalTank1'}],
+#                  'ElectricTanks': [{'Name': 'ElectricTank1'}],
+#                  'ChemicalThrusters': [{'Name': 'ChemicalThruster1', 'Tanks': 'ChemicalTank1'}],
+#                  'ElectricThrusters': [{'Name': 'ElectricThruster1', 'Tanks': 'ElectricTank1'}],
+#                  'SolarPowerSystem': {'Name': 'SolarPowerSystem1'},
+#                  }
+# }
+
 sat_params = {
     'Name': 'DefaultSC',
     'Orbit': {
@@ -11,8 +28,20 @@ sat_params = {
     'DryMass': 100,  # kg
     'Hardware': {'ChemicalTanks': [{'Name': 'ChemicalTank1'}],
                  'ElectricTanks': [{'Name': 'ElectricTank1'}],
-                 'ChemicalThrusters': [{'Name': 'ChemicalThruster1', 'Tanks': 'ChemicalTank1'}],
-                 'ElectricThrusters': [{'Name': 'ElectricThruster1', 'Tanks': 'ElectricTank1'}],
+                 'ChemicalThrusters': [
+                     {
+                         'Name': 'ChemicalThruster1',
+                         'Tanks': ['ChemicalTank1', 'ChemicalTank2'],
+                         'MixRatio': {'ChemicalTank1': 0.5, 'ChemicalTank2': 0.3, 'ChemicalTank3': 0.2}
+                     }
+                 ],
+                 'ElectricThrusters': [
+                     {
+                         'Name': 'ElectricThruster1',
+                         'Tanks': ['ElectricTank1', 'ElectricTank2'],
+                         'MixRatio': {'ElectricTank1': 0.5, 'ElectricTank2': 0.5}
+                     }
+                 ],
                  'SolarPowerSystem': {'Name': 'SolarPowerSystem1'},
                  }
 }
