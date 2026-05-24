51c204b9f3f663f26e3bb42ca8c2480eb963dcd5
weasdown <34220924+weasdown@users.noreply.github.com>
2026-05-24 11:43:54 +0100

    refactor: move Antenna to imager.py and make a subtype of Imager

--- gmatpyplus/hardware/hardware.py
+++ gmatpyplus/hardware/hardware.py
@@ -1,26 +1,14 @@
 from __future__ import annotations
 
-from typing import Union
-
 import numpy as np
 
 import gmatpyplus as gp
 from basics import GmatObject
-from gmatpyplus import gmat
 
 
 ## TODO implement Hardware class
 class Hardware(gp.GmatObject):
     pass
-
-
-class Antenna(GmatObject):
-    def __init__(self, name: str, boresight: np.ndarray | list = np.array([1, 0, 0])):
-        super().__init__('Antenna', name)
-
-        self._boresight = np.array(boresight) if not isinstance(boresight, np.ndarray) else boresight
-
-        raise NotImplementedError
 
 # class Direction:
 #     # TODO move to a more appropriate file
--- gmatpyplus/hardware/imager.py
+++ gmatpyplus/hardware/imager.py
@@ -356,3 +356,12 @@ class Imager(GmatObject):
         self.rotation_matrix = np.array([x, y, z])
 
         self.Initialize()
+
+
+class Antenna(Imager):
+    def __init__(self, name: str, boresight: np.ndarray | list = np.array([1, 0, 0])):
+        super().__init__('Antenna', name)
+
+        self._boresight = np.array(boresight) if not isinstance(boresight, np.ndarray) else boresight
+
+        raise NotImplementedError
