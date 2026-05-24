562e758b3321e8e97e2f8bf0d6f12a18094c7d24
weasdown <34220924+weasdown@users.noreply.github.com>
2026-05-24 13:01:15 +0100

    fix: remove commented Direction class

--- gmatpyplus/hardware/hardware.py
+++ gmatpyplus/hardware/hardware.py
@@ -9,10 +9,3 @@ from basics import GmatObject
 ## TODO implement Hardware class
 class Hardware(gp.GmatObject):
     pass
-
-# class Direction:
-#     # TODO move to a more appropriate file
-#     def __init__(self, x: int | float = 0, y: int | float = 0, z: int | float = 1):
-#         self.x = x
-#         self.y = y
-#         self.z = z
