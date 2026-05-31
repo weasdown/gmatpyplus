d0311677c877f47682cb57cc1393c17f00a9ab26
weasdown <34220924+weasdown@users.noreply.github.com>
2026-05-31 11:33:10 +0100

    refactor: move EndTarget class to new end_target.py

--- gmatpyplus/command/__init__.py
+++ gmatpyplus/command/__init__.py
@@ -1,5 +1,6 @@
 from .branch_command import *
 from .command import *
+from .end_target import *
 from .gmat_command import GmatCommand
 from .propagate import Propagate
 from .solver_branch_command import *
--- gmatpyplus/command/command.py
+++ gmatpyplus/command/command.py
@@ -173,20 +173,6 @@ class EndFiniteBurn(GmatCommand):
         self.Initialize()
 
 
-class EndTarget(BranchCommand):
-    def __init__(self, parent_target: gp.Target, name: str = None):
-        if name is None:
-            name = f'EndTarget_{parent_target.GetName()}'
-
-        super().__init__('EndTarget', name)
-
-    def Insert(self, command: gp.GmatCommand | gmat.GmatCommand,
-               prev: gp.GmatCommand | gmat.GmatCommand = None) -> bool:
-        command = gp.extract_gmat_obj(command)
-        prev = gp.extract_gmat_obj(prev) if prev is not None else None
-        return gp.extract_gmat_obj(self).Insert(command, prev)
-
-
 class Maneuver(GmatCommand):
     def __init__(self, name: str, burn: gp.ImpulsiveBurn | gp.FiniteBurn, spacecraft: gp.Spacecraft,
                  backprop: bool = False):
