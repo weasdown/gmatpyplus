c9bb24f63c074e9496050546103f513698a0826b
weasdown <34220924+weasdown@users.noreply.github.com>
2026-05-31 11:31:21 +0100

    refactor: move Propagate class to new propagate.py

--- gmatpyplus/command/__init__.py
+++ gmatpyplus/command/__init__.py
@@ -1,5 +1,6 @@
 from .branch_command import *
 from .command import *
 from .gmat_command import GmatCommand
+from .propagate import Propagate
 from .solver_branch_command import *
 from .solver_sequence_command import *
