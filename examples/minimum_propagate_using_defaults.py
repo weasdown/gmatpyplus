7f77eb4f788484ef4107e5a873c461e49807aa58
weasdown <34220924+weasdown@users.noreply.github.com>
2026-06-01 12:50:36 +0100

    fix: fix getting of epoch

--- examples/minimum_propagate_using_defaults.py
+++ examples/minimum_propagate_using_defaults.py
@@ -124,4 +124,4 @@ print('Run complete!')
 
 sat = gmat.GetRuntimeObject(sat_ref_name)  # update Spacecraft object now it's been propagated
 print(f'\nState after running mission: {GetState(sat)}')
-print(f'Epoch after running mission: {sat.GetEpoch()}\n')
+print(f'Epoch after running mission: {sat.GetField("Epoch")}\n')
