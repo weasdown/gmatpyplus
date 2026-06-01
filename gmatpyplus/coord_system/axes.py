0a406743ee08a93d8c4feb91d97848727f7fec5a
weasdown <34220924+weasdown@users.noreply.github.com>
2026-06-01 11:18:55 +0100

    docs: add TODO to align Axes class with AxisSystem class in GMAT source code

--- gmatpyplus/coord_system/axes.py
+++ gmatpyplus/coord_system/axes.py
@@ -1,6 +1,7 @@
 from basics import GmatObject
 
 
+# TODO align this class with the GMAT source code's AxisSystem class (including renaming here). This will then become the parent type of the InertialAxes and DynamicAxes types.
 # TODO complete - will be able to create each type of Axes, for use in CoordinateSystem
 class Axes(GmatObject):
     def __init__(self, axes_type: str, name: str):
