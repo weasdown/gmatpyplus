409d9dc819c6b03873f5bb6262c4b1cf6dab8fe5
weasdown <34220924+weasdown@users.noreply.github.com>
2026-06-23 12:41:41 +0100

    feat: add Hardware.location property

--- gmatpyplus/hardware/hardware.py
+++ gmatpyplus/hardware/hardware.py
@@ -10,7 +10,7 @@ class Hardware(GmatObject):
     def __init__(self, obj_type: str, name: str):
         super().__init__(obj_type, name)
 
-        self._location: np.ndarray = np.array([0, 0, 0])
+        self._location: np.ndarray = self.location
 
     # TODO: implement Hardware.GetDirection() method
     def GetDirection(self) -> np.ndarray:
@@ -31,3 +31,9 @@ class Hardware(GmatObject):
     # TODO: implement Hardware.HasFOV() method
     def HasFOV(self) -> bool:
         raise NotImplementedError('Hardware.HasFOV() method is not yet implemented.')
+
+    @property
+    def location(self) -> np.ndarray:
+        """Location of center of the hardware element on the spacecraft, in meters."""
+        self._location = self.GetLocation()
+        return self._location
