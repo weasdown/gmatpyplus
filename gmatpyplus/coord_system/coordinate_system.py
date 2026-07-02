4b91f2fe4afafca8d3678a933a0fb53e5ec9e0f9
weasdown <34220924+weasdown@users.noreply.github.com>
2026-07-02 10:16:11 +0100

    style: remove redundant else

--- gmatpyplus/coord_system/coordinate_system.py
+++ gmatpyplus/coord_system/coordinate_system.py
@@ -65,11 +65,10 @@ class CoordinateSystem(GmatObject):
         if origin not in self._allowed_values['Origin']:
             raise AttributeError(f'Specified origin "{origin}" is not recognized. Please specify one of the '
                                  f'following:\n\t{self._allowed_values["Origin"]}')
-        else:
-            self.origin = gmat.GetObject(origin)  # get current (default) origin
-            # attach new origin to CoordinateSystem
-            self.SetStringParameter(1, self.origin.GetName())  # 1 for ORIGIN_NAME, 2 for J2000_BODY_NAME
-            self.SetRefObject(self.origin, gmat.SPACE_POINT, self.origin.GetName())
+        self.origin = gmat.GetObject(origin)  # get current (default) origin
+        # attach new origin to CoordinateSystem
+        self.SetStringParameter(1, self.origin.GetName())  # 1 for ORIGIN_NAME, 2 for J2000_BODY_NAME
+        self.SetRefObject(self.origin, gmat.SPACE_POINT, self.origin.GetName())
 
         # Parse axes argument
         if axes not in self._allowed_values['Axes']:
